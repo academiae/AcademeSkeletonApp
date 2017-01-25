66857b2b12f0026032d8d2885af4760e8c97de87
gabbydgab <gamba@gabbydgab.com>
2017-01-25 08:10:14 +0800

    fixed configuration

--- config/config.php
+++ config/config.php
@@ -3,16 +3,19 @@
 use Zend\ConfigAggregator\ConfigAggregator;
 use Zend\ConfigAggregator\PhpFileProvider;
 
-$aggregator = new ConfigAggregator(
+$config = new ConfigAggregator(
     [
-        Zend\Expressive\ConfigProvider::class,
-        Zend\Component\ConfigProvider::class,
-        CodingMatters\ExpressiveErrorHandler\ConfigProvider::class,
-        CodingMatters\ExpressiveWebHelper\ConfigProvider::class,
+        \Zend\Router\ConfigProvider::class,
+        \Zend\Validator\ConfigProvider::class,
+        \Zend\Navigation\ConfigProvider::class,
+        \Zend\Expressive\ConfigProvider::class,
+        \Zend\Component\ConfigProvider::class,
+        \CodingMatters\ExpressiveWebHelper\ConfigProvider::class,
+        \CodingMatters\ExpressiveErrorHandler\ConfigProvider::class,
         new PhpFileProvider('config/autoload/{{,*.}global,{,*.}local}.php'),
         new PhpFileProvider('config/development.config.php') // only override if development mode is ENABLED
     ],
     __DIR__ . '/../data/cache/application.config.php'
 );
 
-return $aggregator->getMergedConfig();
+return $config->getMergedConfig();
